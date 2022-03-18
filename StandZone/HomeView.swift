a980c34786691795ac45613c0d49dfac5dde9466
yifanlan <1522254605@qq.com>
2022-03-18 14:50:56 -0400

    Add home page

--- StandZone/HomeView.swift
+++ StandZone/HomeView.swift
@@ -10,12 +10,128 @@ import SwiftUI
 struct HomeView: View {
     @ObservedObject var myController: StandZoneController
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Hello Roxanne").bold()
+                Spacer()
+            }
+            .padding()
+            
+            ZStack {
+                Image("world2")
+                    .resizable()
+                    .frame(width: 146.0, height: 146.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                FrequencyCircle()
+                TimeCircle()
+            }
+            DigitView()
+            Spacer()
+                MiddleView()
+//                BottomView()
+
+        }
+
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(myController: StandZoneController())
+    }
+}
+
+struct FrequencyCircle : View {
+    var body: some View {
+        HStack {
+            Circle()
+               .trim(from: 0.25, to: 1.0)
+               .rotation(.degrees(-90))
+               .stroke(Color.green1 ,style: StrokeStyle(lineWidth: 8, lineCap: .butt))
+               .frame(width: 150, height: 150)
+        }
+        .padding()
+    }
+
+}
+
+struct TimeCircle : View {
+    var body: some View {
+        HStack {
+            Circle()
+               .trim(from: 0.4, to: 1.0)
+               .rotation(.degrees(-90))
+               .stroke(Color.blue1 ,style: StrokeStyle(lineWidth: 8, lineCap: .butt))
+               .frame(width: 134, height: 134)
+        }
+        .padding()
+    }
+
+}
+
+
+
+struct DigitView: View {
+    var body: some View {
+        HStack {
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 10)
+                shape.fill().foregroundColor(.green1).frame(height: 60)
+                VStack {
+                    Text("Stand Frequency").bold()
+                    Text("6/12").bold()
+                }
+            }
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: 10)
+                shape.fill().foregroundColor(.blue1)
+                    .frame(height: 60)
+                VStack {
+                    Text("Stand Time").bold()
+                    Text("90/120 mins")
+                }
+            }
+        }.padding()
+    }
+}
+
+
+struct MiddleView: View {
+    var body: some View {
+        VStack(spacing: 0.0) {
+            Image("background3")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 250)
+                BottomView()
+        }
+
+
+    }
+    
+}
+
+
+
+
+struct BottomView: View {
+    var body: some View {
+        ZStack {
+            let shape = Rectangle()
+            shape.fill().foregroundColor(.green1)
+                .edgesIgnoringSafeArea(.all)
+                .frame(maxHeight: 80)
+
+            HStack {
+                Image(systemName: "person.badge.clock").font(.system(size: 50)).foregroundColor(.white)
+                Image(systemName: "waveform.path.ecg.rectangle").font(.system(size: 50)).foregroundColor(.white)
+                Image(systemName: "gamecontroller").font(.system(size: 50)).foregroundColor(.white)
+                Image(systemName: "gearshape").font(.system(size: 50)).foregroundColor(.white)
+
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+
+
     }
 }
