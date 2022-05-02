a00e1cb45c00738727ae42b42c89be3c6def1ee2
yifanlan <1522254605@qq.com>
2022-05-02 16:27:06 -0400

    add reference

--- StandZone/Data/HealthData.swift
+++ StandZone/Data/HealthData.swift
@@ -1,10 +1,8 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A collection of HealthKit properties, functions, and utilities.
+ This code was adapted from Apple Developer Sample
+ https://developer.apple.com/documentation/healthkit/creating_a_mobility_health_app
+ (Source: https://docs-assets.developer.apple.com/published/ae09943a8b/CreatingAMobilityHealthApp.zip retrieved in April 2022)
 */
-
 import Foundation
 import HealthKit
 
